728af26f59a3b5ae9fb5ccdef6d3dd8af7c37955
Luigi Frunzio <Giggiux@Luigis-MacBook-Pro.local>
2014-11-26 00:56:55 +0100

    too many thing.

--- functions.py
+++ functions.py
@@ -1,29 +1,37 @@
 __author__ = 'Giggiux'
 import re
+mainTagList = ['div','a','table','h1','h2','h3','p','img']
+mainTagDict = {'div':'divisors','a':'links','table':'tables','h1':'h1 headers','h2':'h2 headers','h3':'h3 headers','p':'paragraphs','img':'images'}
 
+# LOADING FUNCTIONS
 def loadFromUrl(page):
-    # while True:
-    #     try:
     import urllib.request
     siteStr = urllib.request.urlopen(page).read().decode('utf-8')
     return siteStr
-        # except:
-        #     print('The url is not valid')
-# loadFromUrl('http://fmpassion.com')
 
 def loadFromFile(page):
     siteStr = open(page, 'r').read()
     return siteStr
-# loadFromFile('prova.html')
 
 def soupify(siteStr):
     from bs4 import BeautifulSoup
     siteSoup = BeautifulSoup(siteStr)
     return siteSoup
 
-def title(siteSoup):
+# BASIC FUNCTIONS
+def makeTitle(siteSoup):
     return siteSoup.title.string
 
+def countMainTags(siteSoup):
+    global mainTagList
+    global mainTagDict
+    countDict = dict()
+    for tag in mainTagList:
+        listTag = siteSoup.find_all(tag)
+        countDict[mainTagDict[tag]] = str(len(listTag))
+    return countDict
+
+# ADVANCED FUNCTIONS
 def linkList(siteSoup):
     if siteSoup.find_all('a') == []:
         return ['']
@@ -31,7 +39,6 @@ def linkList(siteSoup):
         return siteSoup.find_all('a')
 
 def linkDict(linkList):
-    # lst = siteSoup.find_all('a')
     dictLink = dict()
     for link in linkList:
         if re.match('http://.*', str(link.get('href'))):
@@ -44,11 +51,16 @@ def showExternalLink(linkdict):
     for link in linkdict:
         print(str(link) + ' : ' + linkdict[link].strip())
 
+def countOtherTags(siteSoup):
+    global mainTagList
+    countDict = dict()
+    for tag in siteSoup.body.find_all(True):
+        if tag.name not in mainTagList and tag.name not in countDict:
+            listTag = siteSoup.find_all(tag.name)
+            countDict[tag.name] = str(len(listTag))
+    return countDict
 
-# web = soupify(loadFromUrl('http://fmpassion.com'))
-# web = soupify(loadFromFile('prova.html'))
-# print(web.prettify())
-
+# MAIN FUNCTIONS
 def load():
     while True:
         choice = input('Do you want to analyze a file, an url or you want to quit? : ')
@@ -71,5 +83,26 @@ def load():
         else:
             exit()
 
-# web = load()
-# print(type(web.prettify()))
+def header():
+    title = makeTitle(site)
+    maintagsDict = countMainTags(site)
+    print('Page Title is \'' + str(title) + '\'')
+    print('This page has:')
+    for key in maintagsDict:
+        if maintagsDict[key] != '0':
+            print('\'' + maintagsDict[key], key + '\'', '\t', end='')
+
+def advanced():
+    while True:
+        request = input('Do you want ')
+
+
+
+def main():
+    header()
+
+site = load()
+main()
+# print(countOtherTags(site))
+# print(title(site))
+# print(countMainTags(site))
